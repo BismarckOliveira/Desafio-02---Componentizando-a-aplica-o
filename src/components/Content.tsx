8aad4cff0f8d09e28f83a3f073faaea5e9919906
BismarckOliveira <bismarckoliver@hotmail.com>
2021-04-15 10:04:31 -0300

    Primeira Resolução do desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,21 @@
-export function Content() {
-  // Complete aqui
+interface ContentProps {
+  title: string
+}
+
+
+export const Content: React.FC<ContentProps> = (children, props) => {
+ return (
+
+  <div className="container">
+  <header>
+    <span className="category">Categoria:<span> {props.title}</span></span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {children}
+    </div>
+  </main>
+</div>
+ )
 }
